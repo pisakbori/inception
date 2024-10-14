c7fcdba7bc955275868f6c1a83b5aae19572042f
bpisak-l <bpisak-l@student.42.fr>
2024-10-14 22:22:52 +0200

    Mariadb, adminer and nginx together

---
+++ srcs/requirements/mariadb/tools/start.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+chown mysql:mysql /etc/my.cnf.d
+
+mariadb-install-db --user=mysql --datadir=/var/lib/mysql 
+
+mkdir -p /run/mysqld
+
+chown mysql:mysql /run/mysqld
+
+echo ===========================
+
+mysqld --user=mysql --bootstrap --verbose=0  --skip-networking=0 <<EOF
+CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};
+FLUSH PRIVILEGES;
+CREATE USER IF NOT EXISTS '${SQL_USER}'@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';
+FLUSH PRIVILEGES;
+EOF
+
+echo ===========================
+
+# mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown;
+ps
+
+echo ===========================
+
+exec mysqld_safe
